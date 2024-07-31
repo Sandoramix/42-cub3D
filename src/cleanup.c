4a82f061686641b190fac99bcc4ec85b815ce44c
Oleksandr Dudniak <odudniak@student.42firenze.it>
2024-07-31 17:53:47 +0200

    added cleanup stuff

--- src/cleanup.c
+++ src/cleanup.c
@@ -6,7 +6,7 @@
 /*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 17:11:29 by odudniak          #+#    #+#             */
-/*   Updated: 2024/07/31 17:13:37 by odudniak         ###   ########.fr       */
+/*   Updated: 2024/07/31 17:53:35 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,16 @@
 
 int	cleanup(t_var *game, bool should_exit, int statuscode)
 {
-	(void)game;
+	str_freemtx(game->mapinfo.file_content);
+	str_freemtx(game->mapinfo.map);
+
+	if (game->mlx_ptr)
+	{
+		mlx_clear_window(game->mlx_ptr, game->win_ptr);
+		mlx_destroy_window(game->mlx_ptr, game->win_ptr);
+		mlx_destroy_display(game->mlx_ptr);
+		free(game->mlx_ptr);
+	}
 	if (should_exit)
 		exit(statuscode);
 	return (statuscode);
