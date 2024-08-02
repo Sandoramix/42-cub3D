675a742d0bae7e1b0b2ec40559c757465ec7dfb4
Oleksandr Dudniak <odudniak@student.42firenze.it>
2024-08-02 19:36:32 +0200

    undone a bad idea

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 12:43:47 by odudniak          #+#    #+#             */
-/*   Updated: 2024/08/02 18:30:20 by odudniak         ###   ########.fr       */
+/*   Updated: 2024/08/02 19:34:51 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int	main(int ac, char **av)
 {
 	t_var	game;
 
-	game = (t_var){0};
-	parse(&game, ac, av, true);
+	ft_bzero(&game, sizeof(t_var));
+	parse(&game, ac, av);
 	mlx_configuration(&game);
 
 
--- src/parsing/parse.c
+++ src/parsing/parse.c
@@ -6,13 +6,13 @@
 /*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 15:46:08 by odudniak          #+#    #+#             */
-/*   Updated: 2024/08/02 18:35:57 by odudniak         ###   ########.fr       */
+/*   Updated: 2024/08/02 19:35:46 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3D.h>
 
-static int	validate_path(t_var *game, int ac, char **av)
+static int	validate_map_filepath(t_var *game, int ac, char **av)
 {
 	char	*file;
 	int		fd;
@@ -53,6 +53,13 @@ t_cnf	parse_identify_cnf(char *line)
 	return (CNF_UNKNOWN);
 }
 
+static t_state	validate_config_line(t_var *game, char *line)
+{
+	(void)game;
+	(void)line;
+	return (OK);
+}
+
 static t_state	parse_configs(t_var *game)
 {
 	char	**filedata;
@@ -64,35 +71,43 @@ static t_state	parse_configs(t_var *game)
 	{
 		if (str_isblank(filedata[i]))
 			continue ;
-		
+		if (!is_config_missing(game))
+		{
+			if (i > 0 && str_ilen(filedata[i - 1]) == 0)
+				return (OK);
+		}
+		else if (parse_identify_cnf(filedata[i]) == CNF_UNKNOWN)
+			return (ft_perror("Error: '%s': unknown configuration\n",
+					filedata[i]), cleanup(game, true, 1), KO);
+		validate_config_line(game, filedata[i]);
 	}
 	return (OK);
 }
 
-static t_state	parse_content(t_var *game, bool exit_on_err)
+static t_state	parse_content(t_var *game)
 {
 	parse_configs(game);
 	if (is_config_missing(game))
-		return (print_missing_config(game), cleanup(game, exit_on_err, 1), KO);
+		return (print_missing_config(game), cleanup(game, true, 1), KO);
 	// TODO: configuration parsing
 	// TODO: then map parsing
 	(void)game;
 	return (OK);
 }
 
-t_state	parse(t_var *game, int ac, char **av, bool exit_on_err)
+t_state	parse(t_var *game, int ac, char **av)
 {
 	int		file_fd;
 
-	file_fd = validate_path(game, ac, av);
+	file_fd = validate_map_filepath(game, ac, av);
 	game->mapinfo.file_content = ft_readfile(file_fd, false);
 	close(file_fd);
 	if (!game->mapinfo.file_content)
 	{
 		ft_perror("Error: file is empty\n", av[1]),
-		cleanup(game, exit_on_err, 1);
+		cleanup(game, true, 1);
 		return (KO);
 	}
-	parse_content(game, exit_on_err);
+	parse_content(game);
 	return (OK);
 }
