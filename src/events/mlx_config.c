12f31bf3a1979cb1959ff653c1440f6153c6dcdc
Oleksandr Dudniak <odudniak@student.42firenze.it>
2024-08-03 14:35:21 +0200

    removed second initialization

--- src/events/mlx_config.c
+++ src/events/mlx_config.c
@@ -5,16 +5,15 @@ void load_sprites(t_var *game);
 
 void config_event_handling(t_var *game)
 {
-    mlx_hook(game->mlx_win, KeyPress, KeyPressMask, &key_press, &game);
-    mlx_hook(game->mlx_win, KeyRelease, KeyReleaseMask, &key_release, &game);
+    mlx_hook(game->mlx_win, KeyPress, KeyPressMask, &key_press, game);
+    mlx_hook(game->mlx_win, KeyRelease, KeyReleaseMask, &key_release, game);
 	mlx_hook(game->mlx_win, 17, 0, mlx_loop_end, game->mlx);
-	mlx_loop_hook(game->mlx, &game_loop, &game);
+	mlx_loop_hook(game->mlx, &game_loop, game);
 }
 
 
 void mlx_configuration(t_var *game)
 {
-    game->mlx = mlx_init();
     mlx_get_screen_size(game->mlx, &game->dda.screen_size_w_px, &game->dda.screen_size_h_px);
     dbg_printf("screen size, x y: %d %d\n",game->dda.screen_size_w_px, game->dda.screen_size_h_px );
 	game->mlx_win = mlx_new_window(game->mlx, game->dda.screen_size_w_px, game->dda.screen_size_h_px, "UrMom");
