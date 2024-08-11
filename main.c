0d31cd479e7d117a7a6597ff802470cd1190046b
PapaLeoneIV <riccardo.leone97@gmail.com>
2024-08-11 16:29:39 +0200

    [PURGED] hard coded values

--- main.c
+++ main.c
@@ -12,24 +12,6 @@
 
 #include <cub3D.h>
 
-// TODO purge me hahahahah lol
-void init_hardcoded_value(t_var *game)
-{
-	if (ft_isdebug())
-	{
-		dbg_printf("LOADED MAP:\n");
-		ft_putstrmtx(game->mapinfo.map);
-	}
-	ft_putstrmtx(game->mapinfo.map);
-
-	//printf("Player Position x: %.5f\nPlayer Position y: %.5f\n", game->player.x_px, game->player.y_px);
-	//printf("Player Map Position x: %.5f\nPlayer Position y: %.5f\n", game->player.x_px / 64, game->player.y_px / 64);
-	/*gestire direzione il player is facing in base al parsing*/
-
-
-
-}
-
 int main(int ac, char **av)
 {
 	t_var game;
@@ -39,7 +21,6 @@ int main(int ac, char **av)
 	if (!game.mlx)
 		return (pf_errcode(E_MLX), cleanup(&game, true, 1), 1);
 	parse(&game, ac, av);
-	init_hardcoded_value(&game);
 	mlx_setup(&game);
 	render(&game);
 	mlx_loop(game.mlx);
