31d3c74583928078883878b2ac4d586693675669
PapaLeoneIV <riccardo.leone97@gmail.com>
2024-08-15 15:56:24 +0200

    [mouse click] check if inside screen

--- src/mlx_engine/events/mouse_movement_event.c
+++ src/mlx_engine/events/mouse_movement_event.c
@@ -6,28 +6,31 @@
 /*   By: rileone <rileone@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 14:04:32 by rileone           #+#    #+#             */
-/*   Updated: 2024/08/15 15:45:24 by rileone          ###   ########.fr       */
+/*   Updated: 2024/08/15 15:55:28 by rileone          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3D.h>
 
-int on_mousemovement(int button, int x,int y, void *param)
+int on_mouse_click(int button, int x,int y, void *param)
 {
 	t_var *game;
-
+	
 	game = (t_var *)param;
 	(void)button;
 	(void)x;
 	(void)y;
-	printf("Mouse position: %d %d\n", x, y);
-	game->mouse.inside_screen = true;
+	//printf("Mouse position: %d %d\n", x, y);
+	if(x < 0 || y < 0 || x > game->config.win_width || y > game->config.win_height)
+		game->mouse.inside_screen = false;
+	else
+		game->mouse.inside_screen = true;
 	return (OK);
 }
 
 
 void handle_mouse_rotation(t_var *game, t_mouse *mouse)
 {
-	(void)game;
-	(void)mouse;
+	mlx_mouse_get_pos(game->mlx, game->mlx_win,
+		&mouse->pos.x, &mouse->pos.y);
 }
