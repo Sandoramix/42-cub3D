dd1be3b49d30f9631304b54d1b1275723001ebba
PapaLeoneIV <riccardo.leone97@gmail.com>
2024-08-15 15:57:39 +0200

    [mouse hook] compatibiity with other hook

--- src/mlx_engine/game_loop.c
+++ src/mlx_engine/game_loop.c
@@ -6,7 +6,7 @@
 /*   By: rileone <rileone@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/11 23:29:26 by odudniak          #+#    #+#             */
-/*   Updated: 2024/08/15 15:41:24 by rileone          ###   ########.fr       */
+/*   Updated: 2024/08/15 15:54:08 by rileone          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 int	game_loop(t_var *game)
 {
 	delta_time(game, false);
- 	//handle_mouse_rotation(game, &game->mouse);
+ 	handle_mouse_rotation(game, &game->mouse);
 	handle_horizontal_movement(game);
 	handle_vertical_movement(game);
 	handle_vertical_rotation(game);
 	handle_horizontal_rotation(game);
 	handle_player_sprites(game);
 	render_base(game);
-	//render_minimap(game);
+	render_minimap(game);
 	render_sprites(game, &game->config.hands_sprites);
 	mlx_put_image_to_window(game->mlx, game->mlx_win, game->img, 0, 0);
 	if (ft_isdebug())
