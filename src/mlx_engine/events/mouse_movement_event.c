3665330ed68e0e32e81721df88bbc51206a5d9ce
PapaLeoneIV <riccardo.leone97@gmail.com>
2024-08-16 17:14:56 +0200

    [mouse rotation] now the check is done through the diff between prev e actual pos

--- src/mlx_engine/events/mouse_movement_event.c
+++ src/mlx_engine/events/mouse_movement_event.c
@@ -6,7 +6,7 @@
 /*   By: rileone <rileone@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 14:04:32 by rileone           #+#    #+#             */
-/*   Updated: 2024/08/16 15:58:58 by rileone          ###   ########.fr       */
+/*   Updated: 2024/08/16 17:14:16 by rileone          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int on_mouse_click(int button, int x,int y, void *param)
 	(void)button;
 	(void)x;
 	(void)y;
+	(void)game;
 	return (OK);
 }
 
@@ -48,40 +49,61 @@ int is_mouse_centered(t_var *game, t_mouse *mouse)
 	return (!is_mouse_left(game, mouse) && !is_mouse_right(game, mouse));
 }
 
+void choose_mouse_direction(t_var *game, int diff)
+{
+	if (diff > 0)
+	{
+		game->move.mouse_right = true;
+		game->move.mouse_left = false;
+	}
+	else if (diff < 0)
+	{
+		game->move.mouse_left = true;
+		game->move.mouse_right = false;
+	}
+	else
+	{
+		game->move.mouse_left = false;
+		game->move.mouse_right = false;
+	}
+}
+
+void handle_mouse_offscreen(t_var *game, t_mouse *mouse, int diff)
+{
+	(void)diff;
+	if (mouse->pos.x < 100 || mouse->pos.x > game->config.win_width - 100)
+	{
+		mlx_mouse_move(game->mlx, game->mlx_win,
+			game->config.win_width / 2, game->config.win_height / 2);
+		mouse->pos.x = game->config.win_width / 2;
+		mouse->pos.y = game->config.win_height / 2;
+	}
+}
+
 void handle_mouse_rotation(t_var *game, t_mouse *mouse)
 {
-	mlx_mouse_get_pos(game->mlx, game->mlx_win,
-		&mouse->pos.x, &mouse->pos.y);
-	if (!is_mouse_inside_screen(game, mouse))
+	int prev_x;
+	int diff;
+
+	prev_x = mouse->pos.x;
+	mlx_mouse_get_pos(game->mlx, game->mlx_win, &mouse->pos.x, &mouse->pos.y);
+
+	if (is_mouse_inside_screen(game, mouse))
+	{
+		mlx_mouse_hide(game->mlx, game->mlx_win);
+		diff = mouse->pos.x - prev_x;
+		choose_mouse_direction(game, diff);
+		//TODO improve offscreen checks
+			//-ho provato a controllare quanto grande fosse la diff 
+			//per far si che quando c era un movimento di mouse troppo grande il
+			//cursore venisse riportato al centro FAIL
+		handle_mouse_offscreen(game, mouse, diff);
+	}
+	else
 	{
 		game->move.mouse_left = false;
 		game->move.mouse_right = false;
 		mlx_mouse_show(game->mlx, game->mlx_win);
 	}
-	else
-	{
-		//TODO : add rotation speed based on distance from screen center
-		//TODO : decide if i want to keep the mouse inside the screen when playing
-		//TODO : smooth out the rotation with the mouse
-		mlx_mouse_hide(game->mlx, game->mlx_win);
-		if(is_mouse_centered(game, mouse))
-		{
-			game->move.mouse_left = false;
-			game->move.mouse_right = false;
-		}
-		if(is_mouse_left(game, mouse))
-		{
-			game->move.mouse_left = true;
-			game->move.mouse_right = false;
-			mlx_mouse_move(game->mlx, game->mlx_win,
-				game->config.win_width / 2, game->config.win_height / 2);
-		}
-		if(is_mouse_right(game, mouse))
-		{
-			game->move.mouse_right = true;
-			game->move.mouse_left = false;
-			mlx_mouse_move(game->mlx, game->mlx_win,
-				game->config.win_width / 2, game->config.win_height / 2);
-		}
-	}
+		
 }
